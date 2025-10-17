413327c0b0977496c7de50924bf66619c02e401a
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-17 17:43:06 +0530

    updated RewardNFT.sol

--- src/RewardNFT.sol
+++ src/RewardNFT.sol
@@ -2,19 +2,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
-import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
-import {Counters} from "openzeppelin-contracts/utils/Counters.sol";
+import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract RewardNFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+    
+    uint256 private _nextTokenId;
 
     constructor() ERC721("Gasless Reward", "GFT") Ownable(msg.sender) {}
 
     function claimNFT() public {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+       
+        uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
     }
 }
